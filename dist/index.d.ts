88efcfd46a6887534d3b57911f226e30c1f9b273
Bùi Tấn Sang <sang.t.bui@tgl-sol.com>
2024-07-11 15:45:02 +0700

    feat update bundle

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+declare function sum(a: number, b: number): number;
+declare function sub(a: number, b: number): number;
+declare function mult(a: number, b: number): number;
+declare function div(a: number, b: number): number;
